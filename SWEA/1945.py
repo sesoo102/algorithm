21cd6b61c36877c0b119e0dc7bbbf1f6709212f6
sesoo102 <seunsoo102@gmail.com>
2025-02-11 11:09:26 +0900

    Add files via upload

--- SWEA/1945.py
+++ SWEA/1945.py
@@ -9,5 +9,24 @@ for test_case in range(1, T+1):
     d = 0
     e = 0
     while N % 2 == 0:
-        if N % 2 ==0:
-            a+=1
+        a += 1
+        N //= 2
+
+    while N % 3 == 0:
+        b += 1
+        N //= 3
+
+    while N % 5 == 0:
+        c += 1
+        N //= 5
+
+
+    while N % 7 == 0:
+        d += 1
+        N //= 7
+
+    while N % 11 == 0:
+        e += 1
+        N //= 11
+
+    print(f'#{test_case} {a} {b} {c} {d} {e}')
